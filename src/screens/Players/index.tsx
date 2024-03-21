7aac41214a0f5cc9d25b6f7cbfd2b93d3ebfc4ec
Mauricio Bertolucci <github@mauberinformatica.com>
2024-03-21 16:42:06 -0300

    Erro - Forum Rocketseat

---
+++ src/screens/Players/index.tsx
@@ -0,0 +1,82 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
+import { Input } from '@components/Input';
+import { Header } from '@components/Header';
+import { Button } from '@components/Button';
+import { Filter } from '@components/Filter';
+import { Highlight } from '@components/Highlight';
+import { ButtonIcon } from '@components/ButtonIcon';
+import { PlayerCard } from '@components/PlayerCard';
+
+import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
+
+
+export function Players() {
+
+    const [team, setTeam] = useState('TIME A')
+    const [players, setPlayers] = useState(['Mauricio'])
+
+    return (
+        <Container>
+
+            <Header showBackButton />
+
+            <Highlight
+                title='Nome da Turma'
+                subtitle='Adicione a galera e separe os times'
+            />
+
+            <Form>
+
+                <Input
+                    placeholder='Nome do participante'
+                    autoCorrect={false}
+                />
+
+                <ButtonIcon
+                    icon='add'
+                />
+
+            </Form>
+
+            <HeaderList>
+
+                <FlatList
+                    data={['TIME A', 'TIME B']}
+                    keyExtractor={item => item}
+                    renderItem={({ item }) => (
+                        <Filter
+                            title={item}
+                            isActive={item === team}
+                            onPress={() => setTeam(item)}
+                        />
+                    )}
+                    horizontal
+                />
+
+                <NumberOfPlayers>
+                    {players.length}
+                </NumberOfPlayers>
+
+
+            </HeaderList >
+
+            <FlatList
+                data={players}
+                keyExtractor={item => item}
+                renderItem={(item) => (
+                    <PlayerCard
+                        name={item}
+                    />
+                )}
+            />
+
+            <Button
+                title='Remover Turma'
+                type='SECONDARY'
+            />
+
+        </Container >
+    );
+}
